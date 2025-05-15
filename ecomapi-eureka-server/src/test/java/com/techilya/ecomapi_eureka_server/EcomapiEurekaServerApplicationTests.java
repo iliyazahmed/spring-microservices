4e0bbd5fbd1f55391bfbb2afd7731dcb06a2def0
Mohammad Iliyaz <91978876+iliyazahmed@users.noreply.github.com>
2025-05-15 15:05:05 +0530

    Add files via upload

---
+++ ecomapi-eureka-server/src/test/java/com/techilya/ecomapi_eureka_server/EcomapiEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techilya.ecomapi_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomapiEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
