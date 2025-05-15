0a24ba42b7532dfdd4c7fa718f84037ef33578b9
Mohammad Iliyaz <91978876+iliyazahmed@users.noreply.github.com>
2025-05-15 15:05:50 +0530

    Add files via upload

---
+++ ecomapi-apigateway/src/test/java/com/techilya/ecomapi_apigateway/EcomapiApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techilya.ecomapi_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomapiApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
